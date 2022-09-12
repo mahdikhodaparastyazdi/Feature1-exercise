efbd9393039eb3ef091dafe43e47933356186158
unknown <mahdi.kho59@gmail.com>
2022-09-12 11:46:47 +0430

    refactore filename and delete book struct

--- redispublisher/main.go
+++ redispublisher/main.go
@@ -10,19 +10,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type Author struct {
-	Firstname string `json:"firstname"`
-	Lastname  string `json:"lastname"`
-}
-
-//Book Struc(Model)
-type Book struct {
-	ID     string  `json:"id"`
-	Isbn   string  `json:"isbn"`
-	Title  string  `json:"titl"`
-	Author *Author `json:"author"`
-}
-
 type Order struct {
 	Orderid int    `json:"order_id"`
 	Price   int    `json:"price"`
@@ -31,14 +18,12 @@ type Order struct {
 
 var orders []Order
 
-//books //Book
-func createBook(w http.ResponseWriter, r *http.Request) {
+func createOrder(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var order Order
 	_ = json.NewDecoder(r.Body).Decode(&order)
 	payload, _ := json.Marshal(order)
 
-	//book.ID = strconv.Itoa(rand.Intn(1000000))
 	orders = append(orders, order)
 
 	if err := redisClient.Publish(ctx, "send-user-data", payload).Err(); err != nil {
@@ -56,7 +41,7 @@ var redisClient = redis.NewClient(&redis.Options{
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/api/order", createBook).Methods("POST")
+	r.HandleFunc("/api/order", createOrder).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8080", r))
 
 }
